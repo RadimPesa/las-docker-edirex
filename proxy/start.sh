038b4301ea0cf1942a0dca2686fefdaf0efc10db
Radim Peša <2780@mail.muni.cz>
2018-05-25 10:53:15 +0200

    PROXY: serveradmin

--- proxy/start.sh
+++ proxy/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Define FQDN ${HOST}" > /usr/local/apache2/conf/fqdn.conf
-cat /usr/local/apache2/conf/fqdn.conf
+echo "Define EMAILADMIN ${EMAIL_ADMIN_USER}" >> /usr/local/apache2/conf/fqdn.conf
+#cat /usr/local/apache2/conf/fqdn.conf
 
 htpasswd -c -b /usr/local/apache2/conf/.htpasswd administrator ${ADMIN_PASSWORD} 
 
